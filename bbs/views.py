fa42b078307330ba12a7de2f0fbb06e23663fdd4
nullcline <hi.suguru@gmail.com>
2019-02-17 23:54:53 +0900

    first commit

---
+++ bbs/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Article
+
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'message': 'Welcome my BBS',
+        'articles': articles,
+    }
+    return render(request, 'bbs/index.html', context)
